2930fb63b61775946688d24a38d2ed5dc449706e
jaejin1 <opiximeo@gmail.com>
2019-08-25 13:26:12 +0900

    Add informers

---
+++ informers/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"path/filepath"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+
+	// Or uncomment to load specific auth plugins
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/azure"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/openstack"
+)
+
+func main() {
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err)
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+
+	factory := informers.NewFilteredSharedInformerFactory(clientset, 0, "paas", func(opt *metav1.ListOptions) {
+		opt.LabelSelector = "app"
+		//opt.LabelSelector = "app=applabel"
+	})
+	// if you want to get all pods in namespace
+	//factory := informers.NewFilteredSharedInformerFactory(clientset, 0, "paas", func(opt *v1.ListOptions) {})
+	// if you want to get all namespace
+	//factory := informers.NewSharedInformerFactory(clientset, 0)
+
+	informer := factory.Core().V1().Pods().Informer()
+	//informer := factory.Core().V1().Services().Informer()
+	//informer := factory.Batch().V1().Jobs().Informer()
+	//informer := factory.Apps().V1().Deployments().Informer()
+	//...
+
+	stopper := make(chan struct{})
+	defer close(stopper)
+	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			Obj := obj.(metav1.Object)
+			log.Printf("New Pod Added to Store: %s in %s", Obj.GetName(), Obj.GetNamespace())
+		},
+	})
+
+	informer.Run(stopper)
+}
---
+++ informers/main_test.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes/fake"
+	"k8s.io/client-go/tools/cache"
+)
+
+// TestFakeClient demonstrates how to use a fake client with SharedInformerFactory in tests.
+func TestFakeClient(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Create the fake client.
+	client := fake.NewSimpleClientset()
+
+	// We will create an informer that writes added pods to a channel.
+	objs := make(chan *v1.Pod, 1)
+
+	informers := informers.NewSharedInformerFactory(client, 0)
+	podInformer := informers.Core().V1().Pods().Informer()
+
+	podInformer.AddEventHandler(&cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			Obj := obj.(*v1.Pod)
+			t.Logf("pod added: %s/%s", Obj.Namespace, Obj.Name)
+			objs <- Obj
+		},
+	})
+
+	// Make sure informers are running.
+	informers.Start(ctx.Done())
+
+	// This is not required in tests, but it serves as a proof-of-concept by
+	// ensuring that the informer goroutine have warmed up and called List before
+	// we send any events to it.
+	cache.WaitForCacheSync(ctx.Done(), podInformer.HasSynced)
+
+	// Inject an event into the fake client.
+	p := &v1.Pod{ObjectMeta: metav1.ObjectMeta{Name: "my-pod"}}
+	_, err := client.CoreV1().Pods("test-ns").Create(p)
+	fmt.Println("test")
+	if err != nil {
+		t.Fatalf("error injecting pod add: %v", err)
+	}
+
+	select {
+	case obj := <-objs:
+		t.Logf("Got pod from channel: %s/%s", obj.Namespace, obj.Name)
+	case <-time.After(wait.ForeverTestTimeout):
+		t.Error("Informer did not get the added pod")
+	}
+}
